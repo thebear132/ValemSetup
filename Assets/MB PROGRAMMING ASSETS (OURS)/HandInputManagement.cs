04d80d7d1868410265f086f48e4c58a719e45c6d
Bjørn D <drachmann.bjorn@gmail.com>
2021-04-14 22:33:35 +0200

    Hånder virker nu også, forkerte controller af en eller anden grund men tjaaa, det virker da. Skybox også added og flere bolde. Ser pænt fedt ud nu

--- Assets/MB PROGRAMMING ASSETS (OURS)/HandInputManagement.cs
+++ Assets/MB PROGRAMMING ASSETS (OURS)/HandInputManagement.cs
@@ -1,49 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.XR;
 
 public class HandInputManagement : MonoBehaviour
 {
-    private InputDevice RControllerDevice;
-    private InputDevice LControllerDevice;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> ControllerPrefabsList; //Liste over alle controllerprefabs //Mappen Vr Controllers Models
 
+    private InputDevice controllerDevice;
+    private GameObject spawnedController;
 
     private struct ControllerValuesStruct
-    {
+    { //Bare lige de vigtigste
         public Vector2 Primary2DAxis;
         public bool PrimaryButton;
         public bool SecondaryButton;
 
-
         public float Trigger;
         public float Grip;
         public bool TriggerButton;
-
     }
-
-
-    static ControllerValuesStruct RControllerValues;
-    static ControllerValuesStruct LControllerValues;
+    static ControllerValuesStruct ControllerValues; //Et struct til hver controller
 
 
     void Start()
     {
         List<InputDevice> devices = new List<InputDevice>(); //Få alle devices knyttet til Oculus'en
+
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices); //Opdater devices til kun at have den højre controller
+
+        //Debug.Log("Printing all devices attached");
         foreach (var device in devices)
         {
-            Debug.Log(device.name + " | " + device.characteristics);
+            Debug.Log(device.name + " ---> " + device.characteristics); //Bare lige skriv dem alle ud
         }
+        //Debug.Log("Total of " + devices.Count + " devices");
 
-        InputDeviceCharacteristics RControllerCharac = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller; //Få fat i den højre controller.
-        InputDevices.GetDevicesWithCharacteristics(RControllerCharac, devices); //Opdater devices til kun at have den højre controller
-        if (devices.Count > 0)
-            RControllerDevice = devices[0]; //Lav et nyt objekt med den højre controller
 
-        InputDeviceCharacteristics LControllerCharac = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller; //Få fat i den VENSTRE controller.
-        InputDevices.GetDevicesWithCharacteristics(LControllerCharac, devices);
-        if (devices.Count > 0)
-            LControllerDevice = devices[0];
+        if (devices.Count > 0) //Hvis den ikke kan finde nogle, så bare lav den til den første device på listen
+        {
+            controllerDevice = devices[0];
+            GameObject controllerPrefab = ControllerPrefabsList.Find(controller => controller.name == controllerDevice.name);
+            if (controllerPrefab)
+            {
+                spawnedController = Instantiate(controllerPrefab, transform);
+            }
+            else
+            {
+                Debug.LogError("Did not a corresponding controller model");
+                spawnedController = Instantiate(ControllerPrefabsList[0], transform);
+            }
+        }
 
     }
 
@@ -51,24 +60,19 @@ public class HandInputManagement : MonoBehaviour
     void Update()
     {
         //Få thumbstickens værdier som en 2D vector
-        RControllerDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out RControllerValues.Primary2DAxis); //TryGetFeatureValue(Knappen den skal finde, variablen den skal skrive til)
-        LControllerDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out LControllerValues.Primary2DAxis);
+        controllerDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out ControllerValues.Primary2DAxis); //TryGetFeatureValue(Knappen den skal finde, variablen den skal skrive resultatet til)
 
         //Få primary button bool værdi //A
-        RControllerDevice.TryGetFeatureValue(CommonUsages.primaryButton, out LControllerValues.PrimaryButton);
-        LControllerDevice.TryGetFeatureValue(CommonUsages.primaryButton, out LControllerValues.PrimaryButton);
+        controllerDevice.TryGetFeatureValue(CommonUsages.primaryButton, out ControllerValues.PrimaryButton);
 
         //Få secondary button bool værdi //B
-        RControllerDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out LControllerValues.SecondaryButton);
-        LControllerDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out LControllerValues.SecondaryButton);
+        controllerDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out ControllerValues.SecondaryButton);
 
 
         //OUTPUT DEBUG DELEN
-        Debug.Log("2DAxis Right: " + RControllerValues.Primary2DAxis.x + "|" + RControllerValues.Primary2DAxis.y);
-        Debug.Log("2DAxis Left: " + LControllerValues.Primary2DAxis.x + "|" + LControllerValues.Primary2DAxis.y);
-        Debug.Log("Right Primary = " + RControllerValues.PrimaryButton);
-        Debug.Log("Left Primary = " + LControllerValues.PrimaryButton);
-
+        Debug.Log(controllerDevice.name);
+        Debug.Log("2DAxis : " + ControllerValues.Primary2DAxis.x + "|" + ControllerValues.Primary2DAxis.y);
+        Debug.Log("Primary = " + ControllerValues.PrimaryButton);
 
     }
 }
